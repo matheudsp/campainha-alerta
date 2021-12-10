b19f32f45c3811f433bef2719adbffd968b7386d
Matheus Pereira <mdsp.personal@gmail.com>
2021-12-10 12:32:59 -0300

    first commit

--- Alerta.py
+++ Alerta.py
@@ -13,8 +13,8 @@ class EnviarAlerta(Robot):
         emoji_alerta = (emoji.emojize(':warning:'))
         self.msg = 'Há visita(s)!!'+emoji_alerta+'\n'
         self.msg2 = ' está tocando a campainha.'
-        self.TokenBOT = 'seu_token_do_bot'
-        self.UserID = 'seu_id_chat_ou_group_id'  # id do chat pessoal ou id do grupo
+        self.TokenBOT = 'bot_telegram_token'
+        self.UserID = 'idchat ou groupid do seu telegram'  # id do chat pessoal ou id do grupo
         enviarMensagem = 'https://api.telegram.org/bot' + self.TokenBOT + '/sendMessage?chat_id=' + self.UserID + '&parse_mode=Markdown&text=' + str(self.msg) +'\n'+ str(self.v) + str(self.msg2)
 
         alerta = requests.get(enviarMensagem)
